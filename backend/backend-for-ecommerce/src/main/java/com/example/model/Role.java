73ac76a75e9b42333c00d65cffddeb87b84a34e4
makwanadharmik311 <makwanadharmik311@gmail.com>
2025-03-05 22:53:45 +0530

    changed

--- backend/backend-for-ecommerce/src/main/java/com/example/model/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.model;
-
-public enum Role {
-    CUSTOMER, ADMIN
-}
-
- 
--- backend/backend-for-ecommerce/src/main/java/com/example/repository/PaymentInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.PaymentInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentInfoRepository extends JpaRepository<PaymentInfo, Long> {
-}
